a3fca3e74b5596c6c59d4451760b5e72e1a60187
Maximilian Lösch <Maximilian.Loesch@outlook.com>
2022-12-02 19:48:55 +0100

    Second Day Second Puzzle

--- 2/second.exs
+++ 2/second.exs
@@ -1,40 +1,24 @@
 
 defmodule Adventofcode do
-  
-  def build_index(data) do
+
+  def run() do
+    {:ok, f} = File.open("./second_data.txt", [ :utf8 ])
+    data = IO.stream(f, :line)
     list = data |> Enum.to_list
     list |> Enum.reduce( 0, fn elem, acc ->
         case String.trim(elem) do
-          "A X" -> acc + 1 + 3
-          "A Y" -> acc + 2 + 6
-          "A Z" -> acc + 3 + 0
+          "A X" -> acc + 3 + 0
+          "A Y" -> acc + 1 + 3
+          "A Z" -> acc + 2 + 6
           "B X" -> acc + 1 + 0
           "B Y" -> acc + 2 + 3
           "B Z" -> acc + 3 + 6
-          "C X" -> acc + 1 + 6
-          "C Y" -> acc + 2 + 0
-          "C Z" -> acc + 3 + 3
+          "C X" -> acc + 2 + 0
+          "C Y" -> acc + 3 + 3
+          "C Z" -> acc + 1 + 6
           _ -> acc
       end
     end)
-  end
-
-
-  def sum_of_sublist(enumerable, acc \\ 0)
-  def sum_of_sublist([head | tail], acc) do
-    case String.trim(head) do
-      "" -> [acc | sum_of_sublist(tail)]
-      x -> sum_of_sublist(tail, acc + String.to_integer(x))
-    end
-  end
-
-  def sum_of_sublist([], acc) do
-    [acc]
-  end
-
-  def run() do
-    {:ok, data} = File.open("./second_data.txt", [ :utf8 ])
-    build_index IO.stream(data, :line)
   end
 end
 
